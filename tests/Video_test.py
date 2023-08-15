ffd70fd1aca91f4011275bed18cb5af424273061
Joakim Englund <joakimenglund@protonmail.com>
2023-08-15 13:55:37 +0200

    Changed the output file type to mp4.

--- tests/Video_test.py
+++ tests/Video_test.py
@@ -2,6 +2,7 @@ import time
 
 from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder
+from picamera2.outputs import FfmpegOutput
 
 # Setup
 picam2 = Picamera2()
@@ -9,9 +10,10 @@ video_config = picam2.create_video_configuration()
 picam2.configure(video_config)
 
 encoder = H264Encoder(10000000)
+output = FfmpegOutput('test.mp4')
 
-# Start recording video, wait 10 seconds, then stop
-picam2.start_recording(encoder, 'test.h264')
+# Start recording video (as mp4), wait 10 seconds, then stop
+picam2.start_recording(encoder, output)
 time.sleep(10)
 picam2.stop_recording()
 
