649d2589710464b5c30b89a8d6c400f77ff7c348
Jimmy Barr <113925667+JimnyB@users.noreply.github.com>
2023-08-29 09:52:47 +0200

    Update subprogram_status.py
    now 3x subprocesses at a time instead of 1 and added the kill function to terminate threads if necessary

--- tests/subprogram_status.py
+++ tests/subprogram_status.py
@@ -10,24 +10,58 @@ def AddLog(status):
     logFile.flush()
     logFile.close()
 
-# Start the process first time
+# Start the processes first time
 process = subprocess.Popen(['python', 'lektion.py'])
+process2 = subprocess.Popen(['python','lektion1.py'])
+process3 = subprocess.Popen(['python','lektion2.py'])
 AddLog('Started')
 
 while True:
     # Check the state of the process
     status = process.poll()
+    status2 = process2.poll()
+    status3 = process3.poll()
 
     if status != None:
         # Terminated, restart process
         process = subprocess.Popen(['python', 'lektion.py'])
-        print ('Termination code %d, restarted' % (status))
+        print ('Thread 1: Termination code %d, restarted' % (status))
         if status == 0:
-            AddLog('Restarted')
+            AddLog('Thread 1: Restarted')
         else:
-            AddLog('Exception raised, restarted')
+            AddLog('Thread 1: Exception raised, restarted')
         
     else:
         # Still running
-        AddLog('Still running')
-        time.sleep(5)
+        AddLog('Thread 1: still running')
+
+    if status2 != None:
+        # Terminated, restart process
+        process2 = subprocess.Popen(['python', 'lektion1.py'])
+        print ('Thread 2: Termination code %d, restarted' % (status2))
+        if status2 == 0:
+            AddLog('Thread 2: Restarted')
+        else:
+            AddLog('Thread 2: Exception raised, restarted')
+        
+    else:
+        # Still running
+        AddLog('Thread 2: Still running')
+    
+    if status2 != None:
+        # Terminated, restart process
+        process3 = subprocess.Popen(['python', 'lektion2.py'])
+        print ('Thread 3: Termination code %d, restarted' % (status2))
+        if status2 == 0:
+            AddLog('Thread 3: Restarted')
+        else:
+            AddLog('Thread 3: Exception raised, restarted')
+        
+    else:
+    # Still running
+        AddLog('Thread 3: Still running')
+        #process3.kill()
+        #process2.kill()
+        #process.kill()
+        time.sleep(10)
+    
