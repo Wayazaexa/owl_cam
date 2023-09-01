8d844bb68b57e13e7fb86a9088516e35b04b00d3
Raspberry Pi <nope@movant.se>
2023-09-01 15:15:30 +0200

    Cleaned out the repo

--- tests/tobiasflaskltest/flasktest-main/test.py
+++
@@ -1,5 +0,0 @@
-def hello_world():
-    x =2
-    y =3
-    z =x+y
-    return "hellow world"+str(z)
--- tests/tobiasflasktestv2/flasktestV2-main/test.py
+++
@@ -1,6 +0,0 @@
-def hello_world():
-    x = 2
-    y = 3
-    z = x+y
-    return "Hello, world"+str(z)
-
